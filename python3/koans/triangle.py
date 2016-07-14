21dc3575d58c7b56e1a0d80f8ebb57e773fdcaaa
Ryan Tanay <ryantanay@newevolution.org>
2016-07-14 13:55:49 -0500

    Completed triangle project 2
    Also added some docstrings to the methods I created. Split out the one invalid triangle test to its own method

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,12 +17,25 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    if a == b == c:
-        return 'equilateral'
-    elif a == b or b == c or c == a:
-        return 'isosceles'
+    'Given 3 side lengths, returns the type of triangle, \
+    raises TriangleError if the inputs do not form a triangle'
+    if (a and b and c > 0) and triTest(a, b, c):
+        if a == b == c:
+            return 'equilateral'
+        elif a == b or b == c or c == a:
+            return 'isosceles'
+        else:
+            return 'scalene'
     else:
-        return 'scalene'
+        raise TriangleError
+
+def triTest(*args):
+    'Checks that the sides provided create a closed figure \
+    with a contained area'
+    sides = sorted(args)
+    if sides[0] + sides[1] <= sides[2]:
+        return False
+    return True
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
