85bfea0a7e85de8c8ed884966b616011e89a28bb
Ryan Tanay <ryantanay@newevolution.org>
2016-07-14 10:37:53 -0500

    Completed control statements koans

--- python3/koans/about_control_statements.py
+++ python3/koans/about_control_statements.py
@@ -65,13 +65,13 @@ class AboutControlStatements(Koan):
             ("Lancelot", "Blue"),
             ("Galahad", "I don't know!"),
             ("Robin", "Blue! I mean Green!"),
-            ("Arthur", "Is that an African Swallow or Amazonian Swallow?")
+            ("Arthur", "Is that an African Swallow or European Swallow?")
         ]
         result = []
         for knight, answer in round_table:
             result.append("Contestant: '" + knight + "'   Answer: '" + answer + "'")
 
-        text = __
+        text = r'Blue! I mean Green!'
 
         self.assertRegex(result[2], text)
 
